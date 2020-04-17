f0fd8dfaad353566889e6c5910249a28a567cf6e
louiscapelle <louis.capelle@epitech.eu>
2020-04-17 10:49:59 +0200

    positions of buttons has been updated

--- src/pause_menu/init.c
+++ src/pause_menu/init.c
@@ -18,6 +18,10 @@ int create_sprites_pause(game_t *game)
 
 int set_textures_pause(game_t *game)
 {
+    game->pausemenu->main_texture = sfTexture_createFromFile
+                                ("./utils/imgs/menu_button.png", NULL);
+    sfSprite_setTexture(game->pausemenu->main_sprite,
+                        game->pausemenu->main_texture, sfTrue);
     sfSprite_setTexture(game->pausemenu->background_sprite,
                         game->startmenu->background_texture, sfTrue);
     sfSprite_setTexture(game->pausemenu->start_sprite,
@@ -30,8 +34,18 @@ int set_textures_pause(game_t *game)
 int set_positions_pause(game_t *game)
 {
     sfVector2f scale = {0.6, 0.6};
+    sfVector2f scale_s = {0.7, 0.7};
+    sfVector2f pos_1 = {830, 540};
+    sfVector2f pos_2 = {830, 680};
+    sfVector2f pos_3 = {830, 820};
 
     sfSprite_setScale(game->pausemenu->background_sprite, scale);
+    sfSprite_setPosition(game->pausemenu->quit_sprite, pos_1);
+    sfSprite_setPosition(game->pausemenu->start_sprite, pos_2);
+    sfSprite_setPosition(game->pausemenu->main_sprite, pos_3);
+    sfSprite_setScale(game->pausemenu->quit_sprite, scale_s);
+    sfSprite_setScale(game->pausemenu->main_sprite, scale_s);
+    sfSprite_setScale(game->pausemenu->start_sprite, scale_s);
     return 0;
 }
 
