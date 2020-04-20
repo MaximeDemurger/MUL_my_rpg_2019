19b37afb68e852cb95117b613764c7ed9bd9b5e4
Alex Wild <alex.wild@epitech.eu>
2020-04-20 11:16:35 +0200

    adding if enemy in  rage of damage take damage

--- src/player/attack_player.c
+++ src/player/attack_player.c
@@ -7,16 +7,25 @@
 
 #include "my.h"
 
-float vector_dist_player(sfVector2f dir)
+int enemy_inrange(enemy_t *current, float pos_x, float pos_y)
 {
-    float x = dir.x;
-    float y = dir.y;
-    float res = sqrt(x * x + y * y);
-
-    return (res);
+    if (current->pos.x > pos_x - 50 &&
+        current->pos.x < pos_x + 50 &&
+        current->pos.y > pos_y - 50 &&
+        current->pos.y < pos_y + 50)
+        return 1;
+    return 0;
 }
 
-int attack_enemis(play_t *play, utils_t *utils)
+int attack_enemis(play_t *play, utils_t *utils, enemy_t *ene)
 {
+    enemy_t *tmp = ene;
+
+    while (tmp) {
+        if (enemy_inrage(tmp, (float)play->x_play, (float)play->y_play)) {
+            tmp->heart -= 25;
+        }
+        tmp = tmp->next;
+    }
     return 0;
 }
