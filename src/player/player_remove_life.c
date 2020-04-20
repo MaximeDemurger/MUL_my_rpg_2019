5ed50dc1157a52f3746b79122b1ca2c633f73159
Grandjean <camille.grandjean@epitech.eu>
2020-04-20 11:13:19 +0200

    clean code/norme

--- src/player/player_remove_life.c
+++ src/player/player_remove_life.c
@@ -14,16 +14,20 @@ int destroy_heart_next_two(play_t *play, utils_t *utils)
         sfRenderWindow_drawSprite(utils->window, play->heart->empty_two, NULL);
         sfRenderWindow_drawSprite(utils->window, play->heart->empty_three,
             NULL);
-        sfRenderWindow_drawSprite(utils->window, play->heart->empty_four, NULL);
-        sfRenderWindow_drawSprite(utils->window, play->heart->empty_five, NULL);
+        sfRenderWindow_drawSprite(utils->window, play->heart->empty_four,
+        NULL);
+        sfRenderWindow_drawSprite(utils->window, play->heart->empty_five,
+        NULL);
     }
     else if (play->life < 0) {
         sfRenderWindow_drawSprite(utils->window, play->heart->empty_one, NULL);
         sfRenderWindow_drawSprite(utils->window, play->heart->empty_two, NULL);
         sfRenderWindow_drawSprite(utils->window, play->heart->empty_three,
             NULL);
-        sfRenderWindow_drawSprite(utils->window, play->heart->empty_four, NULL);
-        sfRenderWindow_drawSprite(utils->window, play->heart->empty_five, NULL);
+        sfRenderWindow_drawSprite(utils->window, play->heart->empty_four,
+        NULL);
+        sfRenderWindow_drawSprite(utils->window, play->heart->empty_five,
+        NULL);
     }
     return 0;
 }
@@ -34,16 +38,20 @@ int destroy_heart_next(play_t *play, utils_t *utils)
         sfRenderWindow_drawSprite(utils->window, play->heart->one, NULL);
         sfRenderWindow_drawSprite(utils->window, play->heart->two, NULL);
         sfRenderWindow_drawSprite(utils->window, play->heart->three, NULL);
-        sfRenderWindow_drawSprite(utils->window, play->heart->empty_four, NULL);
-        sfRenderWindow_drawSprite(utils->window, play->heart->empty_five, NULL);
+        sfRenderWindow_drawSprite(utils->window, play->heart->empty_four,
+        NULL);
+        sfRenderWindow_drawSprite(utils->window, play->heart->empty_five,
+        NULL);
     }
     if (play->life > 1 && play->life <= 2) {
         sfRenderWindow_drawSprite(utils->window, play->heart->one, NULL);
         sfRenderWindow_drawSprite(utils->window, play->heart->two, NULL);
         sfRenderWindow_drawSprite(utils->window, play->heart->empty_three,
             NULL);
-        sfRenderWindow_drawSprite(utils->window, play->heart->empty_four, NULL);
-        sfRenderWindow_drawSprite(utils->window, play->heart->empty_five, NULL);
+        sfRenderWindow_drawSprite(utils->window, play->heart->empty_four,
+        NULL);
+        sfRenderWindow_drawSprite(utils->window, play->heart->empty_five,
+        NULL);
     } else
         destroy_heart_next_two(play, utils);
     return 0;
@@ -64,7 +72,8 @@ int destroy_heart(play_t *play, utils_t *utils)
         sfRenderWindow_drawSprite(utils->window, play->heart->two, NULL);
         sfRenderWindow_drawSprite(utils->window, play->heart->three, NULL);
         sfRenderWindow_drawSprite(utils->window, play->heart->four, NULL);
-        sfRenderWindow_drawSprite(utils->window, play->heart->empty_five, NULL);
+        sfRenderWindow_drawSprite(utils->window, play->heart->empty_five,
+        NULL);
     } else {
         destroy_heart_next(play, utils);
     }
