4da1ace1b2f424a157c2cfef3ae796bfc248f519
louiscapelle <louis.capelle@epitech.eu>
2020-04-23 14:33:06 +0200

    clear norm

--- src/settings/drag_sound.c
+++ src/settings/drag_sound.c
@@ -22,13 +22,10 @@ void event_sound_bar(game_t *game)
     sfVector2f sprite_pos = sfSprite_getPosition(game->settings->circle_sprite);
     sfVector2f temp = {0, 0};
 
-    printf("mouse: %d, %d\n", pos.x, pos.y);
-    printf("sprite: %f, %f\n", sprite_pos.x, sprite_pos.y);
     if (game->utils->event.type == sfEvtMouseButtonPressed &&
             (pos.x >= sprite_pos.x - 50 && pos.x <= sprite_pos.x + 50) &&
             (pos.y >= 526 && pos.y <= 590) && game->settings->is_dragging == false) {
         game->settings->is_dragging = true;
-        printf("click\n");
     }
     if (game->settings->is_dragging) {
         if (sprite_pos.x > 740 && sprite_pos.x < 1058)
@@ -37,18 +34,5 @@ void event_sound_bar(game_t *game)
     if (game->utils->event.type == sfEvtMouseButtonReleased
         && game->settings->is_dragging == true) {
         game->settings->is_dragging = false;
-        printf("unclick\n");
-    }
-    printf("drag: %d\n", game->settings->is_dragging);
-    if (sprite_pos.x <= 741 && game->settings->is_dragging == false) {
-        printf("je rentre\n");
-        temp.x = sprite_pos.x + 7;
-        temp.y = sprite_pos.y;
-        sfSprite_setPosition(game->settings->circle_sprite, temp);
-    }
-    if (sprite_pos.x >= 1058 && game->settings->is_dragging == false) {
-        temp.x = sprite_pos.x - 7;
-        temp.y = sprite_pos.y;
-        sfSprite_setPosition(game->settings->circle_sprite, temp);
     }
 }
